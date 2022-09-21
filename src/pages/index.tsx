ac2fac9ea7e9877a4f2b929e1f713d187b3b0475
miyoshi <miyoshimas@otsukaele.jp>
2022-09-21 15:58:22 +0900

    2022-9-21-5

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -40,7 +40,7 @@ const View: NextPageWithLayout<
       </Head>
      
       <Heading icon={<RiChatNewLine />} text="home3">
-        ここにダッシュボードを作るよ2。
+        ここにダッシュボードを作るよ3。
       </Heading>
 
       <div>
